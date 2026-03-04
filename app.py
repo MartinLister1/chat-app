4c6a0d026c754d0a7a11fef274034c8a948915c5
martin lister <martinlister1@hotmail.co.uk>
2026-03-04 02:07:15 +0000

    fixed port

--- app.py
+++ app.py
@@ -51,4 +51,6 @@ def handle_message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+   import os
+port = int(os.environ.get('PORT', 5000))
+socketio.run(app, host='0.0.0.0', port=port)
